ef4ec3e931d9fe9cf2cf5d9a6caf2831aef39db4
JabinWashburn <jabinwashburn@gmail.com>
2020-02-27 09:02:49 -0700

    finished project and strech

--- index.js
+++ index.js
@@ -163,8 +163,8 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(list.filter((item, index) => list.indexOf(item) === index))
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
@@ -187,8 +187,10 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
 */
 function getFullNames(runners) {
   const fullNames = [];
-  runners.forEach()
-  return
+  runners.forEach(item => {
+    fullNames.push(`${item.last_name}, ${item.first_name}`)
+  })
+  return fullNames;
 }
 
 /**
@@ -203,8 +205,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const capName = runners.map(item => {
+    return item.first_name.toUpperCase()
+  });
+  return capName; 
 }
 
 /**
@@ -220,8 +225,8 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(item => item.shirt_size === tShirtSize)
 }
 
 /**
@@ -234,8 +239,11 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  let total = runners.reduce((acc, item) => {
+    return acc + item.donation
+  }, 0 );
+  return total
 }
 
 /////////////// CLOSURES ///////////////
@@ -256,10 +264,11 @@ function tallyUpDonations(/* CODE HERE */) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
+  let count = 0;
   function counter() {
-    ++count
+  return count++ ;
   }
+  return counter
   // BROKEN CODE ENDS
 }
 
@@ -283,8 +292,15 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(limit) {
+  let count = -1;
+  function counter(){
+    if (count === limit){
+      count = -1; 
+    }
+    return ++count
+  }
+    return counter
 }
 
 /////////////// END OF CHALLENGE ///////////////
